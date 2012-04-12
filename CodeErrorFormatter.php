ec89134458509780a6e360374ac1b946ad9fd38f
Patrick Allaert <pa@ez.no>
2012-04-12 20:15:28 +0200

    Added: checkstyle format

--- CodeErrorFormatter.php
+++ CodeErrorFormatter.php
@@ -20,35 +20,113 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
-// Usage: CodeErrorFormatter.php /path/to/CodeError.js
+// Usage: CodeErrorFormatter.php [--checkstyle] /path/to/CodeError.js
 
-echo "CodeErrorFormatter 1.0.0 by Patrick Allaert.\n\n";
+$messageMapping = array(
+    "BadPHPIncludeFile" => "Bad include: %s",
+    "PHPIncludeFileNotFound" => "Include not found: %s",
+    "UnknownClass" => "Class %s is unknown",
+    "UnknownBaseClass" => "Base class %s is unknown",
+    "UnknownFunction" => "Unknown function %s",
+    "UseEvaluation" => "Usage of eval()",
+    "UseUndeclaredVariable" => "Variable %s is not declared",
+    "UseUndeclaredGlobalVariable" => "Global variable %s is not declared",
+    "UseUndeclaredConstant" => "Constant %s is not declared",
+    "UnknownObjectMethod" => "Call to unknown method: %s",
+    "InvalidMagicMethod" => "Magic method %s is invalid",
+    "BadConstructorCall" => "Bad call to constructor: %s",
+    "DeclaredVariableTwice" => "Variable is declared twice: %s",
+    "DeclaredConstantTwice" => "Constant is declared twice: %s",
+    "BadDefine" => "Bad define: %s",
+    "RequiredAfterOptionalParam" => "Required parameters after optional parameters: %s",
+    "RedundantParameter" => "Redundant parameter: %s",
+    "TooFewArgument" => "Too few arguments in function or method call: %s",
+    "TooManyArgument" => "Too many arguments in function or method call: %s",
+    "BadArgumentType" => "Bad argument type: %s",
+    "StatementHasNoEffect" => "Statement %s has no effect",
+    "UseVoidReturn" => "Usage of void return value from %s",
+    "MissingObjectContext" => "Trying to use \$this in static context",
+    "MoreThanOneDefault" => "More than one default in switch statement",
+    "InvalidArrayElement" => "Invalid array element: %s",
+    "InvalidDerivation" => "Invalid inheritance: %s",
+    "InvalidOverride" => "Invalid override: %s",
+    "ReassignThis" => "Reassignment of \$this",
+    "MissingAbstractMethodImpl" => "Implementation of abstract methods missing: %s",
+    "BadPassByReference" => "Bad pass-by-reference: %s",
+    "ConditionalClassLoading" => "Class %s is conditionally loaded",
+    "GotoUndefLabel" => "GOTO to invalid label %s",
+    "GotoInvalidBlock" => "GOTO to invalid block: %s",
+    "AbstractProperty" => "Attribute %s is abstract",
+    "UnknownTrait" => "Trait %s is unknown",
+    "MethodInMultipleTraits" => "Method %s is declared in multiple traits",
+    "UnknownTraitMethod" => "Trait method %s is unknown",
+    "InvalidAccessModifier" => "Access modified %s is invalid",
+    "CyclicDependentTraits" => "Cyclic dependency between traits: %s",
+    "InvalidTraitStatement" => "Invalid trait statement: %s",
+    "RedeclaredTrait" => "Trait %s is declared twice",
+    "InvalidInstantiation" => "Invalid instantiation: %s",
+);
 
-if ( !isset( $argv[1] ) || !is_file( $argv[1] ) ) {
-    die( "Usage:\n\tCodeErrorFormatter.php /path/to/CodeError.js\n\n" );
+$options = getopt( ":", array( "checkstyle" ) );
+
+$checkstyleMode = isset( $options["checkstyle"] );
+
+if ( $checkstyleMode ) {
+    $fileArg = 2;
+} else {
+    $fileArg = 1;
+    echo "CodeErrorFormatter 1.1.0 by Patrick Allaert.\n\n";
 }
 
-$data = json_decode(file_get_contents($argv[1]), true);
+if ( !isset( $argv[$fileArg] ) || !is_file( $argv[$fileArg] ) ) {
+    die( "Usage:\n\tCodeErrorFormatter.php [--checkstyle] /path/to/CodeError.js\n\n" );
+}
+
+if ( $checkstyleMode )
+    echo "<checkstyle>\n";
+
+$data = json_decode(file_get_contents($argv[$fileArg]), true);
 
 foreach ( $data[1] as $key => $entries ) {
-    echo $key, "\n================================================================================\n";
+    if ( !$checkstyleMode )
+        echo $key, "\n================================================================================\n";
+
     $previousFile = false;
     foreach ( $entries as $entry ) {
         list( $file, $l1, $c1, $l2, $c2 ) = $entry['c1'];
 
         // Printing filename
         if ( $previousFile !== $file )
-            echo "\n", $file, "\n";
+            if ( $checkstyleMode ) {
+                if ( $previousFile !== false )
+                    echo "  </file>\n";
+                echo '  <file name="', $file, '">', "\n";
+            } else
+                echo "\n", $file, "\n";
 
         // Printing line information
-        echo "\t", $l1, ":", $c1;
-        if ( $l1 !== $l2 || $c1 !== $c2 )
-            echo " -> $l2:$c2";
+        if ( $checkstyleMode ) {
+            echo '    <error line="', $l1, '" message="';
+            printf( $messageMapping[$key], trim( $entry['d'] ) );
+            echo '" severity="error" source="HipHop.PHP.Analysis.', $key,'">', "\n";
+        } else {
+            echo "\t", $l1, ":", $c1;
+            if ( $l1 !== $l2 || $c1 !== $c2 )
+                echo " -> $l2:$c2";
+        }
 
         // Printing details
-        echo "\tdetails: ", trim( $entry['d'] ), "\n";
+        if ( !$checkstyleMode )
+            echo "\tdetails: ", trim( $entry['d'] ), "\n";
 
         $previousFile = $file;
     }
-    echo "\n";
+    if ( $checkstyleMode )
+        if ( $previousFile !== false )
+            echo "  </file>\n";
+    else
+        echo "\n";
 }
+
+if ( $checkstyleMode )
+    echo "</checkstyle>\n";
